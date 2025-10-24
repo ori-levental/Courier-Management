51c41af26628194f52f7473c5d19590886c83182
אורי לונטל <orilevental@gmail.com>
2025-10-24 13:14:28 +0300

    Initial Code + Welcome Function

---
+++ stage0/Program9587.cs
@@ -0,0 +1,20 @@
+﻿namespace stage0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome9587();
+            Welcome3771();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome3771();
+        private static void Welcome9587()
+        {
+            Console.Write("Enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", userName);
+        }
+    }
+}
