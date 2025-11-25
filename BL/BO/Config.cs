b3e95c6b3c2e7ea61f8c3f976e8a0f9c6cf387b2
mordi <mordi@LAPTOP-3ASO2GBI>
2025-11-26 00:30:13 +0200

    add admin manager

---
+++ BL/BO/Config.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    public class Config
+    {
+        public int MaxRange { get; set; }
+
+
+    }
+}
