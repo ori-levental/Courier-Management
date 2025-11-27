8adf9f9e75f1153e467228c1e6b02ae40dbcabb8
אורי לונטל <orilevental@gmail.com>
2025-11-28 00:46:47 +0200

    start of courier implementation

--- BL/BO/Exceptions.cs
+++ BL/BO/Exceptions.cs
@@ -1,14 +1,41 @@
 ﻿namespace BO;
+ [Serializable]
+ public class BLTemporaryNotAvailableException : Exception
+ {
+     public BLTemporaryNotAvailableException() : base() { }
 
-    [Serializable]
-    public class BLTemporaryNotAvailableException : Exception
-    {
-        public BLTemporaryNotAvailableException() : base() { }
+     /// <summary>Initializes a new instance with a specified error message.</summary>
+     public BLTemporaryNotAvailableException(string? message) : base(message) { }
 
-        /// <summary>Initializes a new instance with a specified error message.</summary>
-        public BLTemporaryNotAvailableException(string? message) : base(message) { }
+     /// <summary>Initializes a new instance, wrapping an inner exception.</summary>
+     public BLTemporaryNotAvailableException(string? message, Exception? innerException) : base(message, innerException) { }
+}
 
-        /// <summary>Initializes a new instance, wrapping an inner exception.</summary>
-        public BLTemporaryNotAvailableException(string? message, Exception? innerException) : base(message, innerException) { }
-    }
+[Serializable]
+public class BLInvalidDataException : Exception
+{
+    public BLInvalidDataException() : base() { }
+
+    /// <summary>Initializes a new instance with a specified error message.</summary>
+    public BLInvalidDataException(string? message) : base(message) { }
+
+    /// <summary>Initializes a new instance, wrapping an inner exception.</summary>
+    public BLInvalidDataException(string? message, Exception? innerException) : base(message, innerException) { }
+}
+
+[Serializable]
+public class BLGeneralException : Exception
+{
+    public BLGeneralException() : base() { }
+
+    /// <summary>Initializes a new instance with a specified error message.</summary>
+    public BLGeneralException(string? message) : base(message) { }
+
+    /// <summary>Initializes a new instance, wrapping an inner exception.</summary>
+    public BLGeneralException(string? message, Exception? innerException) : base(message, innerException) { }
+}
+
+
+
+
 
---
+++ BL/BlImplementation/CourierImplementationcs.cs
@@ -0,0 +1,48 @@
+﻿using BlApi;
+using BO;
+
+namespace BlImplementation;
+
+internal class CourierImplementationcs : ICourier
+{
+    public void AddCourier(int requesterId, Courier courier)
+    {
+        try
+        {
+            if (courier == null)
+                throw new BO.BLTemporaryNotAvailableException("Cannot add null object");
+        }
+        catch (BO.BLTemporaryNotAvailableException ex)
+        {
+            throw new BO.BLGeneralException($"ERROR : occurred while try to add new Courier: {ex.Message}");
+        }
+    }
+
+
+
+    ///////////////////----------------------------------------------------------------------------////////////////////
+    public void DeleteCourier(int requesterId, int CourierId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public EmployType EnterToSystem(int id, string password)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<CourierInList> ListOfCourier(int requesterId, bool? isActive, CourierInListEnum? SortBy)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Courier SearchCourier(int requesterId, int CourierId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateCourier(int requesterId, Courier courier)
+    {
+        throw new NotImplementedException();
+    }
+}
